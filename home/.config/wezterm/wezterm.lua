e426655c9531977f1a9e1cfa96f5cb1732e4c1ec
Raymond Shiner <raymondshiner@gmail.com>
2025-08-06 20:14:35 -0700

    update - 08/06/25_20:14

---
+++ home/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'JetBrainsMono Nerd Font',
+  font_size = 11.5,
+  color_scheme = 'Catppuccin Mocha',
+  enable_tab_bar = false,
+  default_prog = { 'wsl.exe', '--distribution', 'Ubuntu' },
+}
+
